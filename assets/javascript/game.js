06b70f083a63231f287d6ad02ae11be3e25d7ec2
William Leung <William Leung>
2017-04-16 13:43:18 -0700

    day 3: logic finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -88,26 +88,6 @@ $(document).ready(function() {
 });
 
 
-
-//create randomize values and assign to each button
-
-//The player will be shown a random number at the start of the game.
-
-//    * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
-//      * Your game will hClasse this amount until the player clicks a crystal.
-//      * When they do click one, update the player's score counter.
-
-//    * The player wins if their total score matches the random number from the beginning of the game.
-
-//    * The player loses if their score goes above the random number.
-
-//    * The game restarts whenever the player wins or loses.
-
-//      * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hClassden values. Of course, the user's score (and score counter) will reset to zero.
-
-//    * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
-
-
-
-// * Each crystal should have a random hClassden value between 1 - 12.
+//Add lose/win sound
+//sound buttons
+//win lose spacings
